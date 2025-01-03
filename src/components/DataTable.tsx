833970abed75a6c740f1268d76f3cf967ede1b6b
Elcid-Chawa <elcidchawa@gmail.com>
2025-01-03 14:26:33 +0200

    refactor data table

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -3,37 +3,72 @@ import { Link } from "react-router-dom";
 
 export default function DataTable({ data }: { data: any[] }) {
   return (
-    <table className="table-fixed border border-collapse w-full">
-      <thead>
+    <table className="w-full">
+      <thead className="bg-gray-50">
         <tr>
-          <th className="py-2 border border-slate-600">ID</th>
-          <th className="py-2 border border-slate-600">Name</th>
-          <th className="py-2 border border-slate-600">Address</th>
-          <th className="py-2 border border-slate-600">No of Parish</th>
-          <th className="py-2 border border-slate-600">Budget Allocation</th>
-          <th className="py-2 border border-slate-600">Action</th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            ID
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Name
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Address
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            No of Parish
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Budget Allocation
+          </th>
+          <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Action
+          </th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className="bg-white divide-y divide-gray-200">
         {data.map((diocese) => (
-          <tr key={diocese.id}>
-            <td className=" px-2 py-1  border border-slate-700">
-              {diocese.id}
+          <tr key={diocese.id} className="hover:bg-gray-50">
+            <td className="px-6 py-4">
+              <div className="text-sm font-medium text-gray-900">
+{diocese.id}
+              </div>
+              
             </td>
-            <td className=" px-2 py-1  border border-slate-700">
-              <Link to={diocese.parish ? `/dioceses/${diocese._id}` : `/parish/${diocese._id}`}>{diocese.name}</Link>
+            <td className="px-6 py-4">
+              <div className="text-sm font-medium text-gray-900">
+<Link
+                to={
+                  diocese.parish
+                    ? `/dioceses/${diocese._id}`
+                    : `/parish/${diocese._id}`
+                }
+              >
+                {diocese.name}
+              </Link>
+              </div>
+              
             </td>
-            <td className=" px-2 py-1  border border-slate-700">
-              {diocese.address}
+            <td className="px-6 py-4">
+              <div className="text-sm font-medium text-gray-900">
+{diocese.address}
+              </div>
+              
             </td>
-            <td className=" px-2 py-1  border border-slate-700">
-              {diocese?.parish?.length ?? 0}
+            <td className="px-6 py-4">
+              <div className="text-sm font-medium text-gray-900">
+{diocese?.parish?.length ?? 0}
+              </div>
+              
             </td>
-            <td className=" px-2 py-1  border border-slate-700">
+            <td className="px-6 py-4">
+              <div className="text-sm font-medium text-gray-900">
+
+              </div>
               {diocese.budgetAllocation}
             </td>
-            <td className="px-2 py-1 flex space-x-2">
-              <button className="bg-blue-500 text-white px-2 py-1 rounded-md">
+            <td className="text-sm font-medium text-gray-900">
+            <button className="bg-blue-500 text-white px-2 py-1 rounded-md">
                 Edit
               </button>
               <button className="bg-red-500 text-white px-2 py-1 rounded-md">
